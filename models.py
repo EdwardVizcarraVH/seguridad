a677fdad7c0bda297890ac7339220cd11c138050
EdwardVizcarraVH <wasabiacme2@gmail.com>
2026-03-24 19:17:35 -0400

    un commit diferente

---
+++ models.py
@@ -0,0 +1,19 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    role = db.Column(db.String(20), default='USER') # USER, ADMIN, AUDITOR
+    otp_secret = db.Column(db.String(32))           # Semilla para MFA
+
+class AuditLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    action = db.Column(db.String(200))
+    ip_address = db.Column(db.String(45))
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
